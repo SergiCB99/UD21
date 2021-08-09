84752ad042c44c7e5654106a3c1ebab591da8fbe
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-08-09 20:13:49 +0200

    Create App.java

---
+++ Ejercicio9/App.java
@@ -0,0 +1,33 @@
+package UD21.Ejercicio9;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+public class App {
+  public static void main(String[] args) {
+    
+    ArrayList<Color> colores = new ArrayList<>();
+	
+	colores.add(Color.red);
+	colores.add(Color.green);
+	colores.add(Color.blue);
+	colores.add(Color.yellow);
+	colores.add(Color.pink);
+	colores.add(Color.white);
+	colores.add(Color.black);
+	colores.add(Color.orange);
+	colores.add(Color.red);
+	colores.add(Color.green);
+	colores.add(Color.blue);
+	colores.add(Color.yellow);
+	colores.add(Color.pink);
+	colores.add(Color.white);
+	colores.add(Color.black);
+	colores.add(Color.orange);
+	
+	//System.out.println(colores.toString());
+	
+	Ex9 app = new Ex9(colores);
+	
+  }
+}
