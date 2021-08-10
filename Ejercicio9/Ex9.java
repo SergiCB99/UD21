a9a048de339df617bb238ebffd9350a8e67f1256
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-08-10 19:26:29 +0200

    Update Ex9.java

--- Ejercicio9/Ex9.java
+++ Ejercicio9/Ex9.java
@@ -13,11 +13,9 @@ import javax.swing.*;
 
 public class Ex9 extends JFrame {
 	
-	JToggleButton[] girados = new JToggleButton[2];
+	protected static JToggleButton[] cartas_giradas = new JToggleButton[2];
 	
-	boolean dos_giradas = false;
-	
-	Random rand = new Random();
+	protected static boolean tirar = true;
 	
 	private JPanel contentPane;
 	public Ex9(ArrayList colores) {
@@ -123,54 +121,66 @@ public class Ex9 extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				
 				JToggleButton source = (JToggleButton)e.getSource();
-				
-				if(girados[1]==null) {
-					if(girados[0]==null) {
-						girados[0]=source;
+					
+					if(tirar) {
+						if(source==TB1) {
+							TB1.setBackground((Color) colores.get(0));
+						}else if(source==TB2) {
+							TB2.setBackground((Color) colores.get(1));
+						}else if(source==TB3) {
+							TB3.setBackground((Color) colores.get(2));
+						}else if(source==TB4) {
+							TB4.setBackground((Color) colores.get(3));
+						}else if(source==TB5) {
+							TB5.setBackground((Color) colores.get(4));
+						}else if(source==TB6) {
+							TB6.setBackground((Color) colores.get(5));
+						}else if(source==TB7) {
+							TB7.setBackground((Color) colores.get(6));
+						}else if(source==TB8) {
+							TB8.setBackground((Color) colores.get(7));
+						}else if(source==TB9) {
+							TB9.setBackground((Color) colores.get(8));
+						}else if(source==TB10) {
+							TB10.setBackground((Color) colores.get(9));
+						}else if(source==TB11) {
+							TB11.setBackground((Color) colores.get(10));
+						}else if(source==TB12) {
+							TB12.setBackground((Color) colores.get(11));
+						}else if(source==TB13) {
+							TB13.setBackground((Color) colores.get(12));
+						}else if(source==TB14) {
+							TB14.setBackground((Color) colores.get(13));
+						}else if(source==TB15) {
+							TB15.setBackground((Color) colores.get(14));
+						}else if(source==TB16) {
+							TB16.setBackground((Color) colores.get(15));
+						}
+					
+						if(cartas_giradas[0]==null) {
+							cartas_giradas[0]=source;
+						}else {
+							cartas_giradas[1]=source;
+							tirar=false;
+						}
+							
+						if(cartas_giradas[1]!=null) {
+							if(cartas_giradas[0] != cartas_giradas[1]) {
+								comprovarColores();
+							}else {
+								System.out.println("No puedes seleccionar la misma carta");
+								girarCartas();
+								resetearArray();
+								tirar=true;
+							}
+						}
+						
 					}else {
-						girados[1]=source;
-						dos_giradas=true;
+						source.setSelected(true);
+						System.out.println("Ya hay 2 cartas giradas");
+						setTimeout(()->resetearArray(), 1500);
 					}
 					
-					if(source==TB1) {
-						TB1.setBackground((Color) colores.get(0));
-					}else if(source==TB2) {
-						TB2.setBackground((Color) colores.get(1));
-					}else if(source==TB3) {
-						TB3.setBackground((Color) colores.get(2));
-					}else if(source==TB4) {
-						TB4.setBackground((Color) colores.get(3));
-					}else if(source==TB5) {
-						TB5.setBackground((Color) colores.get(4));
-					}else if(source==TB6) {
-						TB6.setBackground((Color) colores.get(5));
-					}else if(source==TB7) {
-						TB7.setBackground((Color) colores.get(6));
-					}else if(source==TB8) {
-						TB8.setBackground((Color) colores.get(7));
-					}else if(source==TB9) {
-						TB9.setBackground((Color) colores.get(8));
-					}else if(source==TB10) {
-						TB10.setBackground((Color) colores.get(9));
-					}else if(source==TB11) {
-						TB11.setBackground((Color) colores.get(10));
-					}else if(source==TB12) {
-						TB12.setBackground((Color) colores.get(11));
-					}else if(source==TB13) {
-						TB13.setBackground((Color) colores.get(12));
-					}else if(source==TB14) {
-						TB14.setBackground((Color) colores.get(13));
-					}else if(source==TB15) {
-						TB15.setBackground((Color) colores.get(14));
-					}else if(source==TB16) {
-						TB16.setBackground((Color) colores.get(15));
-					}
-					
-				}
-				
-				if(girados[1]!=null) {
-					//comprovarColores(girados,dos_giradas);
-				}
 			}
           };
           
@@ -193,40 +203,50 @@ public class Ex9 extends JFrame {
         
 	}
 	
-	public static boolean comprovarColores(JToggleButton[] girados,boolean dos_giradas) {
-		boolean acertado = false;
+	public static void comprovarColores() {
 		
-		if(girados[0].getBackground() == girados[1].getBackground()) {
-			setTimeout(()->girados[0].setEnabled(false), 1000);
-			setTimeout(()->girados[1].setEnabled(false), 1000);
-			dos_giradas = false;
-			girados[0]=null;
-			girados[1]=null;
-			acertado=true;
-			System.out.println("Acertado");
+		
+		if(cartas_giradas[0].getBackground()==cartas_giradas[1].getBackground()) {
+			setTimeout(()->desactivarCartas(), 1000);
+			System.out.println("Acierto");
+			setTimeout(()->resetearArray(), 1100);
+			setTimeout(()->tirar=true, 1200);
 		}else {
-			setTimeout(()->girarCartas(girados), 1000);
-			dos_giradas = false;
-			girados[0]=null;
-			girados[1]=null;
-			acertado=false;
-			System.out.println("Fallado");
+			setTimeout(()->girarCartas(), 1000);
+			System.out.println("Fallo");
+			setTimeout(()->resetearArray(), 1100);
+			setTimeout(()->tirar=true, 1200);
 		}
 		
-		return acertado;
 		
 	}
 	
-	public static void girarCartas(JToggleButton[] girados) {
+	public static void desactivarCartas() {
 
-		girados[0].setSelected(true);
-		girados[1].setSelected(true);
+		cartas_giradas[0].setVisible(false);
+		cartas_giradas[1].setVisible(false);
 	
+	}
+	
+	public static void girarCartas() {
+
+		cartas_giradas[0].setSelected(true);
+		cartas_giradas[1].setSelected(true);
+	
+	}
+	
+	public static void resetearArray() {
+
+		cartas_giradas[0]=null;
+		cartas_giradas[1]=null;
+		
 	}
 	
 	//Funcion Copiada
 	public static void setTimeout(Runnable runnable, int delay){
-	    new Thread(() -> {
+		//setTimeout(()->accion que quieras hacer, 1000);
+		
+		new Thread(() -> {
 	        try {
 	            Thread.sleep(delay);
 	            runnable.run();
